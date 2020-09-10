b1567f8a3097f716395ea124bad706de6acc257c
ruthjomo <ruthjomo19@gmail.com>
2020-09-10 13:56:21 +0300

    corrected codes

--- run.py
+++ run.py
@@ -4,82 +4,56 @@ import secrets
 import string
 
 def create_account(user_name, password):
-    '''
-    a function to create a new user account
-    '''
-
-    new_user = User(user_name, password)
+   
+new_user = User(user_name, password)
     return new_user
 
 def save_account(user):
-    '''
-    A function that saves a user
-    '''
+   
 
     User.create_account(user)
 
 def login(user_name, password):
-    '''
-    A function that checks if a user exists and returns a  boolean
-    '''
+    
 
     return User.user_exist(user_name, password)
 
 def create_credentials(account, username, password):
-    '''
-    A function that creates new credentials
-    '''
+   
     new_credentials = Credentials(account, username, password)
     return new_credentials
 
 def save_credentials(credentials):
-    '''
-    A function that saves credentials
-    '''
+   
 
     credentials.save_credentials()
 
 def delete_credentials(credentials):
-    '''
-    A function that deletes credentials
-    '''
+    
 
     credentials.delete_credentials()
 
 def find_credentials(account):
-    '''
-    A function that finds credentials by account and returns them
-    '''
-
+  
     return Credentials.find_by_account(account)
 
 def check_existing_credentials(account):
-    '''
-    A function that checks if credentials exists with that account and returns a boolean
-    '''
+ 
 
     return Credentials.credentials_exist(account)
 
 def display_credentials():
-    '''
-    A function that returns saved credentials
-    '''
+   
 
     return Credentials.display_credentials()
 
 def generate_password(psw_len):
-    '''
-    generate a new password
-    Args:
-    psw_len: preffered password length
-    '''
+   
 
     return "".join(secrets.choice(string.ascii_letters+string.digits) for i in range(psw_len))
 
 def copy_credential(password):
-    '''
-    A function that allows copy and paste password to clipboard
-    '''
+    
 
     return Credentials.copy_password(password)
 
@@ -88,17 +62,17 @@ def main():
     logged_in = False
 
     while True:
-        print("Do you want to sign up or login?")
-        print("Click 's' to sign up or 'l' to login")
+        print("Sign up or login?")
+        print("Click 'Sign Up' to sign up or 'Login' to login")
         answer = input()
 
-        if answer == 'l':
+        if answer == 'Sign Up':
             print('Enter your username: ')
             user_name = input()
             print('Enter your password: ')
             password = input()
 
-            logged_in = login(user_name, password) if answer == 'l' else False
+            logged_in = login(user_name, password) if answer == 'Login' else False
             
 
             while logged_in:
@@ -117,15 +91,15 @@ def main():
 
                     print('Password: ')
                     print('Would you like us to automatically generate you a password? y/n')
-                    ps = input().lower()
+                    psw = input().lower()
 
-                    if ps == 'y':
+                    if psw == 'y':
                         print('Enter your preferred password length')
-                        ps_len = int(input())
-                        password = generate_password(ps_len)
+                        psw_len = int(input())
+                        password = generate_password(psw_len)
                         print(f'Your new password for {account} is {password}')
 
-                    elif ps == 'n':
+                    elif psw == 'n':
                         print('Create your password: ')
                         password = input()
 
@@ -200,7 +174,7 @@ def main():
                         print('-'* 20)
 
                     else:
-                        print(f'{search_account} account Credentials does not exist')
+                        print(f'{search_account}Sorry! account Credentials do not exist')
 
                 elif short_code == 'copy':
 
@@ -230,7 +204,7 @@ def main():
             password = input()
     
             save_account(create_account(user_name, password))
-            print(f'Account for {user_name} has been created')
+            print(f'Account for {user_name} has been created successfully')
             print("\n")
 
         else:
