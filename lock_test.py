27e05193f198d2b64e574b39c40b1fd4b9261825
ruthjomo <ruthjomo19@gmail.com>
2020-09-10 13:30:55 +0300

    Corrected codes

--- lock_test.py
+++ lock_test.py
@@ -5,24 +5,17 @@ import pyperclip
 
 class TestUsers(unittest.TestCase):
 
-    '''
-    Test class that defines test cases for the user class behaviours.
-    Args:
-    unittest.TestCase: TestCase class that helps in creating test cases
-    '''
+    
+    
 
     def tearDown(self):
 
-        '''
-        tearDown method to run after each test  cases.
-        '''
+       
         User.users_list = []
 
     def setUp(self):
 
-        '''
-        Set up method to run before each test cases.
-        '''
+        
         self.new_user = User("Kelvin", "Kelvin78") # create user object
 
 
